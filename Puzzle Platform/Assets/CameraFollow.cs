0fac51b7bc9fdb4f6e49b750d6b85d5cf193a756
SamuelSemlan <semlan12@live.com>
2018-03-09 14:56:09 +0100

    Add game

---
+++ Puzzle Platform/Assets/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+
+    public Vector3 offset;
+    public float smoothSpeed = 0.12f;
+
+    private void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
+        
+    }
+}
---
+++ Puzzle Platform/Assets/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody rb;
+
+    [Header("Movement")]
+    [SerializeField] float movementSpeed;
+    [SerializeField] float jumpingForce;
+
+    [Header("Other")]
+    public LayerMask groundLayer;
+    public CapsuleCollider col;
+
+	
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+
+	}
+
+    void FixedUpdate()
+    {
+        Move();
+        Jump();
+    }
+
+    private void Move()
+    {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 direction = input.normalized;
+        Vector3 velocity = direction * movementSpeed;
+
+        //velocity.y = rb.velocity.y;
+
+        rb.MovePosition(rb.position + velocity * Time.deltaTime);
+    }
+
+
+    private bool IsGrounded()
+    {
+        return Physics.CheckCapsule(col.bounds.center, new Vector3(col.bounds.center.x,
+         col.bounds.min.y, col.bounds.center.z), col.radius * .2f, groundLayer);
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            rb.AddForce(Vector3.up * jumpingForce * Time.deltaTime, ForceMode.Impulse);
+        }
+    }
+
+}
