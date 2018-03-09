36c0d7d378707b0bade318f7f9d3e8ca26e3ad8f
SamuelSemlan <semlan12@live.com>
2018-03-09 16:36:05 +0100

    Test

--- Puzzle Platform/Assets/PlayerController.cs
+++ Puzzle Platform/Assets/PlayerController.cs
@@ -10,10 +10,6 @@ public class PlayerController : MonoBehaviour {
     [SerializeField] float movementSpeed;
     [SerializeField] float jumpingForce;
 
-    [Header("Other")]
-    public LayerMask groundLayer;
-    public CapsuleCollider col;
-
 	
 	void Start () {
         rb = GetComponent<Rigidbody>();
@@ -37,16 +33,9 @@ public class PlayerController : MonoBehaviour {
         rb.MovePosition(rb.position + velocity * Time.deltaTime);
     }
 
-
-    private bool IsGrounded()
-    {
-        return Physics.CheckCapsule(col.bounds.center, new Vector3(col.bounds.center.x,
-         col.bounds.min.y, col.bounds.center.z), col.radius * .2f, groundLayer);
-    }
-
     private void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(Vector3.up * jumpingForce * Time.deltaTime, ForceMode.Impulse);
         }
